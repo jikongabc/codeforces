95b6b5db708a8b549dbe199f7f551327a186c8dd
jikong <jikong_2zzh@qq.com>
2024-05-19 21:39:01 +0800

    1

---
+++ 14_J.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+const int N = 1e9 + 10;
+int n;
+const int dx[] = {-3, -1, 1, 3};
+const int dy[] = {0, 2, 2, 0};
+
+int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    int T;
+    cin >> T;
+    while(T--){
+        cin >> n;
+
+    }
+    return 0;
+}
